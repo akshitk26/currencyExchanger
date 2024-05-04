12e674a782073b61a7ac12c9e9d70446127145b1
darxx26 <akshit2k26@gmail.com>
2024-05-03 22:11:38 -0400

    adsfasdf

---
+++ currencyExchanger/src/components/MoneyLabel.jsx
@@ -0,0 +1,12 @@
+
+function MoneyLabel({currency, value}) {
+
+    return (
+        <>
+        <h1>Currency: {currency} </h1>
+        <h2>Value: {value}</h2>
+        </>
+    );
+}
+
+export default MoneyLabel;
