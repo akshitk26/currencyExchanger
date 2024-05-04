38ed98361c52958ed8b7939077edcc1363dc1eee
darxx26 <akshit2k26@gmail.com>
2024-05-04 00:26:39 -0400

    header

--- currencyExchanger/src/App.jsx
+++ currencyExchanger/src/App.jsx
@@ -1,39 +1,39 @@
-import { useState } from 'react'
-import MoneyLabel from './components/MoneyLabel'
+import { useState } from "react";
+
+import Header from "./components/Header";
+import styles from "./styles.module.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-    <h1>rizzler</h1>
-    <MoneyLabel currency="USD" value="80 rupees"/>
-    </>
-  
-  )
+    <div>
+      <Header></Header>
+    </div>
+  );
 }
 
-export default App
+export default App;
 
 // <div className="App">
-    //   <div>
-    //     <a href="https://vitejs.dev" target="_blank">
-    //       <img src="/vite.svg" className="logo" alt="Vite logo" />
-    //     </a>
-    //     <a href="https://reactjs.org" target="_blank">
-    //       <img src={reactLogo} className="logo react" alt="React logo" />
-    //     </a>
-    //   </div>
-    //   <h1>Vite + React</h1>
-    //   <div className="card">
-    //     <button onClick={() => setCount((count) => count + 1)}>
-    //       count is {count}
-    //     </button>
-    //     <p>
-    //       Edit <code>src/App.jsx</code> and save to test HMR
-    //     </p>
-    //   </div>
-    //   <p className="read-the-docs">
-    //     Click on the Vite and React logos to learn more
-    //   </p>
-    // </div>
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="/vite.svg" className="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://reactjs.org" target="_blank">
+//       <img src={reactLogo} className="logo react" alt="React logo" />
+//     </a>
+//   </div>
+//   <h1>Vite + React</h1>
+//   <div className="card">
+//     <button onClick={() => setCount((count) => count + 1)}>
+//       count is {count}
+//     </button>
+//     <p>
+//       Edit <code>src/App.jsx</code> and save to test HMR
+//     </p>
+//   </div>
+//   <p className="read-the-docs">
+//     Click on the Vite and React logos to learn more
+//   </p>
+// </div>
---
+++ currencyExchanger/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import styles from "./Header.module.css";
+
+function Header() {
+  return (
+    <div className={styles.header}>
+      <h1>Currency Converter</h1>
+      <h2>Github: </h2>
+    </div>
+  );
+}
+
+export default Header;
